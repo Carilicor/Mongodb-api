12d4f24ae7f8481e1cdda1097947bb80105fb17b
Cari Licor <Carilicor@gmail.com>
2022-10-20 11:45:52 -0400

    mongo db api

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { addNewFurniture, getAllFurniture, findFurnitureByModel } from './src/furniture.js'
+
+const PORT = 3030
+const app = express()
+app.use(express.json())
+
+// app.get('/furniture', (req, res) => res.send('furniture'))
+app.post('/furniture', addNewFurniture);
+app.get('/furniture', getAllFurniture)
+app.get('/furniture/:search', findFurnitureByModel)
+
+app.listen(PORT, () => console.log(`Listening on http://localhost${PORT}...`))
